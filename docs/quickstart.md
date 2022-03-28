8a9a90c2077e7b698e6bda2560a154d2980b233f
Julia Martin <juliadmartin720@gmail.com>
2022-03-28 14:22:38 -0700

    CSS changes and update first few docs

--- docs/quickstart.md
+++ docs/quickstart.md
@@ -1,7 +1,19 @@
 ---
 sidebar_position: 1
 ---
-# Quick Start
-Clone the `waypost-docker` Github repository from this link: https://github.com/waypost-io/waypost-docker
+# Quickstart
+The fastest way to get up and running with Waypost is to download/run the Docker container.
 
-Run `docker-compose up` and you're all set!
+Clone the `waypost-docker` Github repository ([https://github.com/waypost-io/waypost-docker](https://github.com/waypost-io/waypost-docker))
+
+```
+git clone https://github.com/waypost-io/waypost-docker.git
+cd waypost-docker
+```
+
+Run `docker-compose up --build`
+
+Visit `http://localhost:3000` to view the Waypost UI and start adding feature flags!
+
+
+![Example UI](../static/img/ui/empty_dashboard.jpg)
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -14,11 +13,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+          <Link className="button button--secondary button--lg" to="/">Read the Case Study</Link>
         </div>
       </div>
     </header>
@@ -33,7 +28,7 @@ export default function Home() {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        {/* <HomepageFeatures /> */}
       </main>
     </Layout>
   );
